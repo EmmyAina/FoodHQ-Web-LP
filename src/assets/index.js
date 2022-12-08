67606726fb3ffacb4664f4f61ba40024e5d21e05
iyanufanoro <iyanufanoro@gmail.com>
2022-12-08 15:53:04 +0100

    FoodHQ web

---
+++ src/assets/index.js
@@ -0,0 +1,35 @@
+import homeHero from "./home_hero.png";
+import homeCards from "./home_cards.png";
+import feature from "./feature.png";
+import expo from "./expo.png";
+import expo02 from "./expo02.png";
+import javascript from "./javascript.png";
+import react from "./react.png";
+import scene from "./scene.png";
+import mockup from "./mockup.png";
+import intro from "./intro.png"
+import feedback from "./feedback.png"
+import alternative from "./14pro.png"
+import threeStack from "./3stack.png"
+import five from "./five.png"
+import wallet from "./wallet_payment.png"
+
+
+
+export default {
+  homeHero,
+  homeCards,
+  feature,
+  expo,
+  expo02,
+  javascript,
+  react,
+  scene,
+  mockup,
+  intro,
+  feedback,
+  alternative,
+  threeStack,
+  five,
+  wallet
+};
