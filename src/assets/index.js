d40544d7cf6f672e37f2c8ce239229d357c2d67b
EmmyAina <ainae07@gmail.com>
2022-12-09 12:43:48 +0100

    app store images

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,29 +7,31 @@ import javascript from "./javascript.png";
 import react from "./react.png";
 import scene from "./scene.png";
 import mockup from "./mockup.png";
-import intro from "./intro.png"
-import feedback from "./feedback.png"
-import alternative from "./14pro.png"
-import threeStack from "./3stack.png"
-import five from "./five.png"
-import wallet from "./wallet_payment.png"
-
-
+import intro from "./intro.png";
+import feedback from "./feedback.png";
+import alternative from "./14pro.png";
+import threeStack from "./3stack.png";
+import five from "./five.png";
+import wallet from "./wallet_payment.png";
+import appleStore from "./appleStore.svg";
+import playStore from "./playStore.png";
 
 export default {
-  homeHero,
-  homeCards,
-  feature,
-  expo,
-  expo02,
-  javascript,
-  react,
-  scene,
-  mockup,
-  intro,
-  feedback,
-  alternative,
-  threeStack,
-  five,
-  wallet
+	homeHero,
+	homeCards,
+	feature,
+	expo,
+	expo02,
+	javascript,
+	react,
+	scene,
+	mockup,
+	intro,
+	feedback,
+	alternative,
+	threeStack,
+	five,
+	wallet,
+	appleStore,
+	playStore,
 };
