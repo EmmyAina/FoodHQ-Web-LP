e9777979feeda70daf802e8206c903aad51fb6d6
EmmyAina <ainae07@gmail.com>
2022-12-09 12:52:00 +0100

    add app store components

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -2,30 +2,39 @@ import React from "react";
 
 import styles from "../styles/Global";
 import assets from "../assets";
+import StoreButton from "./StoreButton";
 
 const Download = () => {
-  return (
-    <div className={`${styles.section} ${styles.bgWhite}`}>
-      <div className={`${styles.subSection} flex-col text-center`}>
-        <div>
-          <h1 className={`${styles.h1Text} ${styles.blackText}`}>
-            Download the Source Code
-          </h1>
-          <p className={`${styles.pText} ${styles.blackText}`}>
-            Get the full source code on GitHub
-          </p>
+	// const iOSUrl = "";
+	// const androidUrl = "";
+	return (
+		<div className={`${styles.section} ${styles.bgWhite}`}>
+			<div className={`${styles.subSection} flex-col text-center`}>
+				<div>
+					<h1 className={`${styles.h1Text} ${styles.blackText}`}>
+						Download the App
+					</h1>
+				</div>
+				<div className={styles.flexCenter}>
+					<img
+						src={assets.five}
+						alt="download_png"
+						className={styles.fullImg}
+					/>
         </div>
-        <button className={styles.btnPrimary}>Source Code</button>
-        <div className={styles.flexCenter}>
-          <img
-            src={assets.five}
-            alt="download_png"
-            className={styles.fullImg}
-          />
-        </div>
-      </div>
-    </div>
-  );
+        
+				<StoreButton
+					assetUrl={assets.appleStore}
+					link={"https://twitter.com"}
+				/>
+
+				<StoreButton
+					assetUrl={assets.playStore}
+					link={"https://twitter.com"}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Download;
